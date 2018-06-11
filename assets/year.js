1da94ab8b6ac83704d58b8f3de47994d9be56baa
Ophir Lifshitz <hangfromthefloor@gmail.com>
2018-06-11 05:06:40 -0400

    Jank pinning feature (#3)

--- assets/year.js
+++ assets/year.js
@@ -4,3 +4,53 @@ document.getElementById('collapse').onclick = function() {
 	this.innerText =
 		(this.innerText == 'Show fewer rows') ? 'Show more rows' : 'Show fewer rows';
 }
+
+function mapPins(f) {
+	return Array.from(document.getElementsByClassName('pin')).map(f);
+}
+
+function getFromLocalStorage() {
+	try {
+		var a = JSON.parse(window.localStorage['pinned']);
+
+		mapPins(function (p) {
+			if (a.includes(p.dataset.region)) {
+				p.dataset.toggle = 'toggle';
+				p.innerText = '★';
+			}
+			else {
+				p.closest('tr').classList.add('toggle');
+			}
+		});
+	} catch (e) {}
+}
+
+function setLocalStorage() {
+	try {
+		var a = {};
+		mapPins(function (p) {
+			if (p.dataset.toggle == 'toggle')
+				a[p.dataset.region] = 0;
+		});
+		window.localStorage['pinned'] = JSON.stringify(Object.keys(a));
+	} catch (e) {}
+}
+
+window.onload = function () {
+	getFromLocalStorage();
+
+	mapPins(function(el) {
+		el.onclick = function(e) {
+			var on = (el.dataset.toggle == 'toggle');
+			var els = document.querySelectorAll('.pin[data-region="' + el.dataset.region + '"]');
+
+			els.forEach(function(el_) {
+				el_.dataset.toggle = (on) ? '' : 'toggle';
+				el_.innerText = (on) ? '☆' : '★';
+				el_.closest('tr').classList[(on) ? 'add' : 'remove']('toggle');
+			})
+			setLocalStorage();
+			e.preventDefault();
+		};
+	});
+}
